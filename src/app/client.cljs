30c25341ee94657c98f076ef3c5066170442722f
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-09-25 12:18:42 +0530

    definitely making progress with rendering the correct parts of the data tree! - so so happy today :)

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -3,6 +3,7 @@
     ;; project libs
     [app.utils :as utils :refer [clog]]
     [app.pathom :as p :refer [spacex-api]]
+    [app.temp-db :refer [latest-launch]]
 
     ;; external libs
     [clojure.string :as str]
@@ -20,19 +21,91 @@
 ;; UTILS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; SingleLaunch Component
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(defsc SingleLaunch [this props]
+;  {:query              []
+;   :initial-state      {}
+;   :initLocalState     (fn [this]
+;                         (clog {:message "[SingleLaunch]: InitLocalState" :color "teal"}))
+;   :componentDidMount  (fn [this]
+;                         (let [p (comp/props this)]
+;                           (clog {:message "[SingleLaunch] MOUNTED" :props p :color "green"})))
+;   :componentDidUpdate (fn [this]
+;                         (let [p (comp/props this)]
+;                           (clog {:message "[SingleLaunch]: UPDATED" :color "blue" :props p})))}
+;  (comp/fragment
+;    (dom/div :.ui.segment "Single Launch" props)))
+;
+;(def ui-single-launch (comp/factory SingleLaunch))
+;
+;
+;(comment
+;
+;  (reset! latest-launch-data {})
+;
+;  (comp/transact! APP [(update-latest-launch)])
+;
+;  @latest-launch-data
+;
+;  (merge/merge-component! SingleLaunch @latest-launch-data)
+;
+;  (app/schedule-render! APP)
+;
+;  )
+;
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; SingleLaunch Component
+;; LaunchFirstStage Component
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO This is the UI part of the component
-(defsc SingleLaunch [this props]
-  {:query         []
-   :initial-state {}}
+(defsc LaunchFirstStage [this {:keys [] :as props}]
+  {#_#_:query []
+   #_#_:ident []
+   :initial-state      {}
+   :initLocalState     (fn [this]
+                         (clog {:message "[LaunchFirstStage]: InitLocalState" :color "teal"}))
+   :componentDidMount  (fn [this]
+                         (let [p (comp/props this)]
+                           (clog {:message "[LaunchFirstStage] MOUNTED" :props p :color "green"})))
+   :componentDidUpdate (fn [this]
+                         (let [p (comp/props this)]
+                           (clog {:message "[LaunchFirstStage]: UPDATED" :color "blue" :props p})))}
   (comp/fragment
-    ()))
 
-(def ui-single-launch (comp/factory SingleLaunch))
+    (dom/div :.ui.segment "Flight number ")
+    (dom/div :.ui.segment "Video link ")))
+
+
+(def ui-launch-first-stage (comp/factory LaunchFirstStage {:keyfn :spacex.core/core-serial}))
+
+
+(comment
+
+  (comp/transact! APP [(update-latest-launch)])
+
+  @latest-launch-data
+
+  (reset! latest-launch-data {})
+
+  (reset! (::app/state-atom APP) @latest-launch-data)
+
+  (reset! (::app/state-atom APP) {})
+
+  (app/current-state APP)
+
+  (reset! (::app/state-atom APP) {:one 1})
+
+  (app/schedule-render! APP)
+
+  )
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -45,23 +118,74 @@
           (spacex-api {} [:spacex/latest-launch])))
 
 
-(defsc LatestLaunch [this props]
-  {:query         []
-   :initial-state {}}
-  (dom/button :.ui.button.primary
-              {:onClick #(comp/transact! this [(fetch-latest-launch)])}
-              "SpaceX: Fetch latest launch!"))
+(def latest-launch-data (atom {}))
 
-(def ui-latest-launch (comp/factory LatestLaunch))
+(m/defmutation update-latest-launch [_]
+  (action [{:keys [state]}]
+          (clog {:message "[LatestLaunch] MUTATION update-latest-launch" :color "magenta" :props state})
+          (reset! latest-launch-data latest-launch)))
+
+(comment
+  (comp/transact! APP [(update-latest-launch)])
+  @latest-launch-data
+
+  (app/schedule-render! APP)
+  )
+
+(defsc LatestLaunch [this {:keys [spacex.launch/flight-number
+                                  spacex.launch.links/video-link
+                                  spacex.launch.first-stage/cores] :as props}]
+  {#_#_:query []
+   #_#_:ident []
+   :initial-state      {}
+   :initLocalState     (fn [this]
+                         (clog {:message "[LatestLaunch]: InitLocalState" :color "teal"}))
+   :componentDidMount  (fn [this]
+                         (let [p (comp/props this)]
+                           (clog {:message "[LatestLaunch] MOUNTED" :props p :color "green"})))
+   :componentDidUpdate (fn [this]
+                         (let [p (comp/props this)]
+                           (clog {:message "[LatestLaunch]: UPDATED" :color "blue" :props p})))}
+  (comp/fragment
+
+    (dom/div :.ui.segment "Flight number " flight-number)
+    (dom/div :.ui.segment "Video link " video-link)
+    (ui-launch-first-stage cores)))
+
+
+(def ui-latest-launch (comp/factory LatestLaunch {:keyfn :spacex.launch/flight-number}))
+
+
+(comment
+
+  (comp/transact! APP [(update-latest-launch)])
+
+  @latest-launch-data
+
+  (reset! latest-launch-data {})
+
+  (reset! (::app/state-atom APP) @latest-launch-data)
+
+  (reset! (::app/state-atom APP) {})
+
+  (app/current-state APP)
+
+  (reset! (::app/state-atom APP) {:one 1})
+
+  (app/schedule-render! APP)
+
+  )
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROOT Component
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsc Root [this props]
-  {:query              []
-   :initial-state      {}
+(defsc Root [this {:keys [:spacex/latest-launch] :as props}]
+  {#_#_:query []
+   #_#_:ident []
 
+   :initial-state      {}
    :initLocalState     (fn [this]
                          (clog {:message "[Root]: InitLocalState" :color "teal"}))
    :componentDidMount  (fn [this]
@@ -72,7 +196,30 @@
                            (clog {:message "[Root]: UPDATED" :color "blue" :props p})))}
   (comp/fragment
     (dom/h1 :.ui.header "Hello, Fulcro!")
-    (ui-latest-launch)))
+    (ui-latest-launch latest-launch)))
+
+
+(comment
+
+  (comp/transact! APP [(update-latest-launch)])
+
+  @latest-launch-data
+
+  (reset! latest-launch-data {})
+
+  (reset! (::app/state-atom APP) @latest-launch-data)
+
+  (reset! (::app/state-atom APP) {})
+
+  (app/current-state APP)
+
+  (reset! (::app/state-atom APP) {:one 1})
+
+  (app/schedule-render! APP)
+
+  )
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; APP definition and init
@@ -92,8 +239,6 @@
   ;; js hard reset
   (.reload js/location true)
 
-  (::app/state-atom APP)
-
   (-> APP
       (::app/state-atom)
       deref)
@@ -102,5 +247,7 @@
 
   (reset! (::app/state-atom APP) {})
 
-  (app/current-state APP))
+  (app/current-state APP)
+
+  )
 
